7208f44e41202f37b19ca5bfd96332ef6c98c6ac
Saar Haber <37675905+saarhaber@users.noreply.github.com>
2019-03-13 21:02:58 -0400

    Counting Valleys

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//My code
+int countingValleys(int n, string s) {
+int level = 0;
+int valley = 0;
+bool prev;
+bool curn;
+
+
+if (n > 1000000) {
+    return 0;
+}
+if (s[0] == 'D') {
+    level--;
+    }
+if (s[0] == 'U') {
+  level++;
+}
+if (level > 0) {
+    prev = true;
+}
+if (level < 0) {
+    prev = false;
+}
+s = s.substr(1, s.length()-1);
+
+    while (!s.empty()) {
+      if (s[0] == 'D') {
+        level--;
+      }
+      if (s[0] == 'U') {
+        level++;
+      }
+      if (level >= 0) {
+          curn = true;
+      }
+      if (level < 0) {
+          curn = false;
+      }
+      if (prev == false && curn == true) {
+          valley++;
+      }
+      prev = curn;
+      s = s.substr(1);
+      if (level > n/2 || level*(-1) > n/2) {
+          return 0;
+      }
+    }
+
+
+return valley;
+}
+
+//HakerRank's code
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string s;
+    getline(cin, s);
+
+    int result = countingValleys(n, s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
